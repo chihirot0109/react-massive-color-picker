1071ed84720204bfdaeeaf77fcade958d7641eae
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-07 19:12:24 +0900

    added copy overlay animation

--- src/component/ColorBox.js
+++ src/component/ColorBox.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import '../css/ColorBox.css'
 
 class ColorBox extends Component {
-  render() {
-    const style = {
-      backgroundColor: this.props.color.color,
-    };
+  constructor(props){
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
 
+  changeCopyState(){
+    this.setState({ copied: true }, () => {
+     setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
+
+  render() {
+    const color  = this.props.color.color;
+    const { copied } = this.state;
+    const style = {
+      backgroundColor: color
+    };
+    
     return(
-    <div className="ColorBox" style={style}>
-      <p>{this.props.color.name}</p>
-    </div>
+      <CopyToClipboard text={color} onCopy={this.changeCopyState} >
+        <div className="ColorBox" style={style} > 
+          <div className={`copy-overlay ${ copied && "show"}`} style={style} />
+          <h1 className={`copy-msg ${ copied && "show"}`}>copied {color}</h1>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{this.props.color.name}</span>
+            </div>
+            <button className="copy-button">copy</button>
+          </div>
+          <span className="see-more">More</span>
+        </div>
+      </CopyToClipboard>
     );
   }
 }
--- src/component/Palette.js
+++ src/component/Palette.js
@@ -6,11 +6,11 @@ class Palette extends Component {
   render() {
       let colors = this.props.colors;
     return(
-    <div className="Palette">
-        <div className="Palette-colors">
-          {colors.map(color => <ColorBox color={color} />)}
-        </div>
-    </div>
+      <div className="Palette">
+          <div className="Palette-colors">
+            {colors.map(color => <ColorBox color={color} />)}
+          </div>
+      </div>
     );
   }
 }
