9d33d8cc72c7ed25199f9650eb016f0d69f1e59b
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-08 14:55:09 +0900

    added color scales

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import Palette from './component/Palette'
 import seedColors from './component/seedColors'
+import { getNewPalette } from './component/colorHelper'
 
 class App extends Component {
   render() {
+    const colors = getNewPalette(seedColors[4]);
+    console.log(colors);
     return(
       <Palette {...seedColors[4]} />
     );
