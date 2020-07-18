ebbb70dbd4632afb01a0fafaf0be18dc66f02e3e
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-18 22:18:17 +0900

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom'
 import Palette from './component/Palette'
 import seedColors from './component/seedColors'
 import { getNewPalette } from './component/colorHelper'
@@ -6,7 +7,11 @@ import { getNewPalette } from './component/colorHelper'
 class App extends Component {
   render() {
     return(
-      <Palette palette={getNewPalette(seedColors[4])} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1> }/>
+        <Route exact path="/palette/:id" render={() => <h1>PALETTE GOES HERE</h1>} />
+      </Switch>
+      /* <Palette palette={getNewPalette(seedColors[4])} /> */
     );
   }
 }
