c7256d64a07ae51b210f427be9595b6bbf149a5a
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-19 20:59:02 +0900

    display colors for each link on root

--- src/component/MiniPalette.js
+++ src/component/MiniPalette.js
@@ -8,28 +8,49 @@ const styles = {
     color: "white"
   },
   colors: {
-    backgroundColor: "grey"
+    height: "150px",
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: "5px",
+    overflow: "hidden"
   },
   title: {
     display: "flex",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
   },
   emoji: {
-    margin: "0 1rem"
+    margin: "0 1rem",
+    fontSize: "1rem"
+  },
+  miniColor: {
+    display: "inline-block",
+    height: "25%",
+    width: "20%",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-4px;"
   }
 }
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div
+      className={classes.miniColor}
+      style={{backgroundColor: color.color}}
+      key={color.name}
+    />
+  ));
   return (
     <div className={classes.root}>
       <div className={classes.colors}>
+        {miniColorBoxes}
+      </div>
         <h5 className={classes.title}>
           {paletteName}
           <span className={classes.emoji}>{emoji}</span>
         </h5>
-      </div>
     </div>
   );
 }
