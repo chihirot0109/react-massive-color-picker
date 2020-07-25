0fc798ad20d592f4fa6df17fd99273b72ce79c5d
Chihiro Tsuda <chihirot0109@gmail.com>
2020-07-25 14:58:31 +0900

    display different color text according to the luminance

--- src/component/ColorBox.js
+++ src/component/ColorBox.js
@@ -27,14 +27,15 @@ class ColorBox extends Component {
         <div className="ColorBox" style={{ background }} > 
           <div className={`copy-overlay ${ copied && "show"}`} style={{background}}/>
             <div className={`copy-msg ${ copied && "show"}`}>
-              <h1>Copied!</h1>
-              <p>{background}</p>
+              <div className={luminance > 0.2 ? "text" : "text dark"}>
+                <h1>Copied {background}</h1>
+              </div>
             </div>
           <div className="copy-container">
             <div className={luminance > 0.2 ? "box-content" : "box-content dark"}>
               <span>{name}</span>
             </div>
-            <button className="copy-button">copy</button>
+            <button className={luminance > 0.2 ? "copy-button" : "copy-button dark"}>copy</button>
           </div>
           { showingFullPalette &&
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
